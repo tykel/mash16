060a9a3a3bba2f9711d1f267bfab0817588925c8
timothy.kelsall@gmail.com <timothy.kelsall@gmail.com@f32040c0-604e-ac78-71ca-de615562ae79>
2011-10-24 08:34:16 +0000

    Added license information, and some comments.
    git-svn-id: https://mash16.googlecode.com/svn/trunk@3 f32040c0-604e-ac78-71ca-de615562ae79

--- GPU/SfmlGPU.h
+++ GPU/SfmlGPU.h
@@ -1,3 +1,19 @@
+/*
+	Mash16 - an open-source C++ Chip16 emulator
+    Copyright (C) 2011 Tim Kelsall
+
+    Mash16 is free software: you can redistribute it and/or modify it under the terms 
+	of the GNU General Public License as published by the Free Software Foundation, 
+	either version 3 of the License, or  (at your option) any later version.
+
+    Mash16 is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; 
+	without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR 
+	PURPOSE.  See the GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License along with this program.
+	If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #ifndef _SFML_GPU_H
 #define _SFML_GPU_H
 
