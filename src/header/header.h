9b5cff45f7169ac1384ab3cd8debf05a01394985
timothy.kelsall@gmail.com <timothy.kelsall@gmail.com@f32040c0-604e-ac78-71ca-de615562ae79>
2012-05-19 11:44:17 +0000

    Imported rom-header API from patch16.
    git-svn-id: https://mash16.googlecode.com/svn/trunk@31 f32040c0-604e-ac78-71ca-de615562ae79

---
+++ src/header/header.h
@@ -0,0 +1,24 @@
+#define CH16_HEADER_SIZE 0x10
+
+#include <stdint.h>
+
+#pragma pack(push,1)
+typedef struct ch16_header
+{
+    uint32_t magic;
+    uint8_t  reserved;
+    uint8_t  spec_ver;
+    uint32_t rom_size;
+    uint16_t start_addr;
+    uint32_t crc32_sum;
+
+} ch16_header;
+
+typedef struct ch16_rom
+{
+    ch16_header header;
+    uint8_t*    data;
+
+} ch16_rom;
+#pragma pack(pop)
+
